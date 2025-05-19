f705b02ac185209f2bfac0a4fe55dcf313ad6eaf
Jamir <yasmirteyez533@gmail.com>
2025-05-18 19:18:49 -0600

    proyectoRGSTRACTUALIZADO

---
+++ BASEDEDATOS/Firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgciBsgqIa3tfhf26D2HcmkvbO6jXSRIM",
+  authDomain: "registro-ad8c8.firebaseapp.com",
+  projectId: "registro-ad8c8",
+  storageBucket: "registro-ad8c8.firebasestorage.app",
+  messagingSenderId: "149627099023",
+  appId: "1:149627099023:web:7609c05343fda376d6ddfa"
+};
+
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+
+export default appFirebase;
